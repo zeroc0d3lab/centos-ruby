208f3ef259f5a38fe290b725e3be1a3dd7a49f81
ZeroC0D3 Team <zeroc0d3.0912@gmail.com>
2017-10-15 21:54:17 +0700

    Update installation gems & ruby packages

--- rootfs/opt/install_ruby.sh
+++ rootfs/opt/install_ruby.sh
@@ -1,35 +1,94 @@
 #!/bin/sh
 
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
+DEFAULT_VERSION='2.4.2'
 RUBY=`which ruby`
-RUBY_V=`ruby -v`
+RUBY_V=`$RUBY -v`
 GEM=`which gem`
 BUNDLE=`which bundle`
 
-if [ "${RUBY_PACKAGE}" = "rbenv" ]
-then
-  #-----------------------------------------------------------------------------
-  # Install Ruby with rbenv (default)
-  #-----------------------------------------------------------------------------
-  git clone https://github.com/rbenv/rbenv.git $HOME/.rbenv \
+logo() {
+  echo "--------------------------------------------------------------------------"
+  echo "  __________                  _________ _______       .___________        "
+  echo "  \____    /___________  ____ \_   ___ \\   _  \    __| _/\_____  \  LAB  "
+  echo "    /     // __ \_  __ \/  _ \/    \  \//  /_\  \  / __ |   _(__  <       "
+  echo "   /     /\  ___/|  | \(  <_> )     \___\  \_/   \/ /_/ |  /       \      "
+  echo "  /_______ \___  >__|   \____/ \______  /\_____  /\____ | /______  /      "
+  echo "          \/   \/                     \/       \/      \/        \/       "
+  echo "--------------------------------------------------------------------------"
+  echo " Date / Time: $DATE"
+}
+
+check_version() {
+  if [ "${RUBY_VERSION}" = "" ]
+    ${RUBY_VERSION} = $DEFAULT_VERSION
+  fi
+}
+
+load_env() {
+  echo ""
+  echo "--------------------------------------------------------------------------"
+  echo "## Load Environment: "
+  echo "   $HOME/.bashrc"
+  source ~/.bashrc
+}
+
+install_ruby() {
+  if [ "${RUBY_PACKAGE}" = "rbenv" ]
+  then
+    #-----------------------------------------------------------------------------
+    # Install Ruby with rbenv (default)
+    #-----------------------------------------------------------------------------
+    git clone https://github.com/rbenv/rbenv.git $HOME/.rbenv \
     && git clone https://github.com/rbenv/ruby-build.git $HOME/.rbenv/plugins/ruby-build \
     && $HOME/.rbenv/bin/rbenv install ${RUBY_VERSION} \
     && $HOME/.rbenv/bin/rbenv global ${RUBY_VERSION} \
     && $HOME/.rbenv/bin/rbenv rehash \
     && $HOME/.rbenv/shims/ruby -v
-else
-  #-----------------------------------------------------------------------------
-  # Install Ruby with rvm (alternatives)
-  #-----------------------------------------------------------------------------
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 \
+  else
+    #-----------------------------------------------------------------------------
+    # Install Ruby with rvm (alternatives)
+    #-----------------------------------------------------------------------------
+    gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 \
     && curl -sSL https://get.rvm.io | sudo bash -s stable \
     && sudo usermod -a -G rvm root \
     && sudo usermod -a -G rvm docker \
-    && source $HOME/.bashrc \
+    && source ~/.bashrc \
     && /usr/local/rvm/bin/rvm install ${RUBY_VERSION} \
     && /usr/local/rvm/bin/rvm use ${RUBY_VERSION} --default \
     && /usr/bin/ruby -v
-fi
+  fi
+}
 
-source $HOME/.bashrc
+check(){
+  echo ""
+  echo "--------------------------------------------------------------------------"
+  echo "## Ruby Version: "
+  echo "   $RUBY_V"
+  echo "--------------------------------------------------------------------------"
+  echo "## Path Ruby: "
+  echo "   $RUBY"
+  echo "--------------------------------------------------------------------------"
+  echo "## Path Gem: "
+  echo "   $GEM"
+}
 
-$GEM install bundle
+install_bundle() {
+  echo ""
+  echo "--------------------------------------------------------------------------"
+  echo "## Install Bundle: "
+  echo "   $GEM install bundle"
+  $GEM install bundle
+}
+
+main() {
+  logo
+  check_version
+  install_ruby
+  load_env
+  check
+  install_bundle
+}
+
+### START HERE ###
+main
--- rootfs/root/gems.sh
+++ rootfs/root/gems.sh
@@ -1,25 +1,67 @@
 #!/bin/sh
 
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
 RUBY=`which ruby`
-RUBY_V=`ruby -v`
+RUBY_V=`$RUBY -v`
 GEM=`which gem`
 BUNDLE=`which bundle`
 
-source $HOME/.bashrc
+logo() {
+  echo "--------------------------------------------------------------------------"
+  echo "  __________                  _________ _______       .___________        "
+  echo "  \____    /___________  ____ \_   ___ \\   _  \    __| _/\_____  \  LAB  "
+  echo "    /     // __ \_  __ \/  _ \/    \  \//  /_\  \  / __ |   _(__  <       "
+  echo "   /     /\  ___/|  | \(  <_> )     \___\  \_/   \/ /_/ |  /       \      "
+  echo "  /_______ \___  >__|   \____/ \______  /\_____  /\____ | /______  /      "
+  echo "          \/   \/                     \/       \/      \/        \/       "
+  echo "--------------------------------------------------------------------------"
+  echo " Date / Time: $DATE"
+}
 
-echo '-------------------------------------------------------------'
-echo '## Ruby version: '
-echo $RUBY_V
-echo '-------------------------------------------------------------'
-echo '## Path ruby in folder: '
-echo $RUBY
-echo '-------------------------------------------------------------'
-echo '## Path gem in folder: '
-echo $GEM
+load_env() {
+  echo ""
+  echo "--------------------------------------------------------------------------"
+  echo "## Load Environment: "
+  echo "   $HOME/.bashrc"
+  source ~/.bashrc
+}
 
-echo ''
-echo 'Begin Installation'
-echo '-------------------------------------------------------------'
+check(){
+  echo ""
+  echo "--------------------------------------------------------------------------"
+  echo "## Ruby Version: "
+  echo "   $RUBY_V"
+  echo "--------------------------------------------------------------------------"
+  echo "## Path Ruby: "
+  echo "   $RUBY"
+  echo "--------------------------------------------------------------------------"
+  echo "## Path Gem: "
+  echo "   $GEM"
+}
 
-$GEM install bundle
-$BUNDLE install
+install_bundle() {
+  echo ""
+  echo "--------------------------------------------------------------------------"
+  echo "## Install Bundle: "
+  echo "   $GEM install bundle"
+  $GEM install bundle
+}
+
+install_package() {
+  echo ""
+  echo "--------------------------------------------------------------------------"
+  echo "## Install Package: "
+  echo "   $BUNDLE install"
+  $BUNDLE install
+}
+
+main() {
+  logo
+  load_env
+  check
+  install_bundle
+  install_package
+}
+
+### START HERE ###
+main
